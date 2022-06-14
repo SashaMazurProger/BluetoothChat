2264e988a56e400389077d5b9cdf73769f8dd48a
alexmazur <mazur.sasha.moo@gmail.com>
2022-06-14 23:41:24 +0300

    [ADD] add test libraries

--- app/src/test/java/com/sashamprog/bluetoothchat/ExampleUnitTest.kt
+++ app/src/test/java/com/sashamprog/bluetoothchat/ExampleUnitTest.kt
@@ -1,16 +1,43 @@
 package com.sashamprog.bluetoothchat
 
+import android.content.Context
+import android.os.Environment
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.mock
+import org.mockito.junit.MockitoJUnitRunner
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
+@RunWith(MockitoJUnitRunner::class)
 class ExampleUnitTest {
+
+    @Mock
+    private lateinit var context: Context
+
+    @Before
+    fun init() {
+        context = mock {
+
+        }
+    }
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun check_string_isEmpty() {
+        print(Environment.getDataDirectory().absolutePath)
+        assertTrue("".isEmpty())
     }
 }
